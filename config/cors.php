09cb1fd4aeadea52f5a0ced0fe34c4f3d6901565
thanhhoa3514 <ngothanhhoa3514@gmail.com>
2025-12-15 09:08:21 +0700

    feat: update cors config this cause block to frontend

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,16 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8080', 'http://localhost:5173'], // Add your frontend URL here
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://localhost:8080',
+        'https://monochrome-news.vercel.app',
+    ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https://.*\.vercel\.app$#',
+    ],
 
     'allowed_headers' => ['*'],
 
